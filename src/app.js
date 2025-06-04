378818dc6782e6a359b27cc41fdb6e2aa3ffb56e
Maulik Kadeval <maulikkadeval@Mauliks-MacBook-Air.local>
2025-06-04 13:44:41 +0530

    Day 2 App

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Devops Intern Day 3');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
