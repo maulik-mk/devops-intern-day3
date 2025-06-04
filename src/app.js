24caadbe9954ace6c0516f2a4873782efcfe0d64
Maulik Kadeval <maulikkadeval@Mauliks-MacBook-Air.local>
2025-06-04 19:41:07 +0530

    Monitoring

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('./logger');
+const client = require('prom-client');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+client.collectDefaultMetrics();
+
 app.get('/', (req, res) => {
   res.send('Hello, Devops Intern Day 3');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 if (require.main === module) {
